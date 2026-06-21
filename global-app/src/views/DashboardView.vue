388aab4f6cb6d6d50adf869e4393bfc5acf008ad
Emperor401 <your-github-email@gmail.com>
2026-06-21 19:17:53 +0100

    fix: rename Wallet button to Digital, route to /digital page

--- global-app/src/views/DashboardView.vue
+++ global-app/src/views/DashboardView.vue
@@ -40,11 +40,11 @@
           </svg>
           Email
         </button>
-        <button class="ha" @click="router.push('/billing')">
+        <button class="ha" @click="router.push('/digital')">
           <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round">
             <polyline points="23 6 13.5 15.5 8.5 10.5 1 18"/><polyline points="17 6 23 6 23 12"/>
           </svg>
-          Wallet
+          Digital
         </button>
       </div>
 
